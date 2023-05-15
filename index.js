b77d62f828ff94c8bf3a7f761757eed244c680cd
GwanJinHan <42578300+GwanJinHan@users.noreply.github.com>
2023-05-15 23:24:21 +0900

    230515

--- index.js
+++ index.js
@@ -1,59 +1,26 @@
-const axios = require("axios");
-const { ProxyAgent } = require("proxy-agent");
 const puppeteer = require("puppeteer");
-const cheerio = require("cheerio");
-const targetURL = "https://www.example.com"; // 크롤링할 대상 URL
-/*
-const proxyList = [
-  'http://proxy1.example.com:8080',
-  'http://proxy2.example.com:8080',
-  // 추가 프록시 서버 주소...
-];
 
-async function fetchDataWithProxy(proxyURL) {
-  try {
-    const agent = new ProxyAgent(proxyURL);
-    const response = await axios.get(targetURL, { httpsAgent: agent });
-
-    // 여기서 데이터 크롤링을 수행하거나 원하는 작업을 수행합니다.
-    // response.data를 사용하여 웹 페이지의 데이터를 추출합니다.
-
-    console.log('크롤링 완료:', targetURL);
-  } catch (error) {
-    console.error('크롤링 실패:', targetURL, error);
-  }
-}
-
-async function runCrawler() {
-  const promises = proxyList.map(fetchDataWithProxy);
-  await Promise.all(promises);
-
-  console.log('모든 크롤링 작업 완료');
-}
-
-runCrawler();
-*/
-
-const [latitude, longitude] = [37.60128, 127.0644736];
-
-const CrawlYogiyo = async () => {
+const result = {};
+const CrawlYogiyo = async (i = 1) => {
   const browser = await puppeteer.launch({ headless: false }); //headless : "new" 로 나중에 바꿔주자
   const page = await browser.newPage();
-  // grant permission
+
   const context = browser.defaultBrowserContext();
   await context.overridePermissions("https://www.yogiyo.co.kr/mobile/#/", [
     "geolocation",
   ]);
-  //set the location
+
   await page.setGeolocation({ latitude: 37.60128, longitude: 127.0644736 });
-  //open url
+
   await page.goto(
     "https://www.yogiyo.co.kr/mobile/#/서울특별시/000000/1인분주문/"
   );
-  await page.reload();
 
+  await page.waitForTimeout(1000);
+  await page.click("#button_search_address > button.btn.btn-default.ico-pick");
+  await page.waitForTimeout(1000);
   await page.waitForSelector(".restaurant-name");
-  // 무한 스크롤 로직 추가
+
   let previousHeight;
   while (true) {
     const currentHeight = await page.evaluate(
@@ -68,66 +35,67 @@ const CrawlYogiyo = async () => {
     await page.evaluate(
       "window.scrollTo(0, document.documentElement.scrollHeight)"
     );
-    await page.waitForTimeout(2000); // 페이지가 추가적인 컨텐츠를 로드할 시간을 기다립니다.
+    await page.waitForTimeout(1000);
   }
-  const menuItems = await page.evaluate(() => {
-    const items = Array.from(document.querySelectorAll(".restaurant-name"));
-    return items.map((item) => item.innerText);
+
+  await page.evaluate("window.scrollTo(0, 0)");
+
+  const itemsLength = await page.evaluate(() => {
+    const items = Array.from(
+      document.querySelectorAll(
+        "#content > div > div:nth-child(4) > div > div.restaurant-list > .col-sm-6"
+      )
+    );
+    return items.length;
   });
-  console.log(menuItems.length);
+  console.log(itemsLength);
+
+  while (i < itemsLength + 1) {
+    if (i % 8 === 0) {
+      await browser.close();
+      return CrawlYogiyo(i);
+    }
+    await page.click(
+      `#content > div > div:nth-child(4) > div > div.restaurant-list > div:nth-child(${i})`
+    );
+    await page.waitForTimeout(2000);
+    const name = await page.evaluate(
+      `document.querySelector(
+        "#content > div.restaurant-detail.row.ng-scope > div.col-sm-8 > div.restaurant-info > div.restaurant-title > span"
+      ).innerText`
+    );
+    const titleIimgUrl = await page.evaluate(
+      `document
+        .querySelector(
+          "#content > div.restaurant-detail.row.ng-scope > div.col-sm-8 > div.restaurant-info > div.restaurant-content > div:nth-child(1)"
+        )
+        .style.backgroundImage.split('"')[1]`
+    );
+    const score = await page.evaluate(
+      `document.querySelector(
+        "#content > div.restaurant-detail.row.ng-scope > div.col-sm-8 > div.restaurant-info > div.restaurant-content > ul > li:nth-child(1) > span"
+      ).innerText`
+    );
+    const minimumOrderedPrice = await page.evaluate(
+      `document.querySelector(
+        "#content > div.restaurant-detail.row.ng-scope > div.col-sm-8 > div.restaurant-info > div.restaurant-content > ul > li:nth-child(3) > span"
+      ).innerText`
+    );
+    const menus = {};
+    const menusCategoryLength = await page.evaluate(
+      `document.querySelectorAll("#menu > .panel").length - 2`
+    );
+    /*  for (i = 2; i < menusCategoryLength; i ++) {
+      document.querySelector(`#menu > div > div:nth-child(${i})`)
+      const categoryName = 
+    } */
+    console.log(name, i);
+    await page.goBack();
+    i += 1;
+    await page.waitForTimeout(2000);
+  }
 
   // await browser.close();
 };
 
 CrawlYogiyo();
-
-// const pageDown = async (page) => {
-//   const scrollHeight = "document.body.scrollHeight";
-//   let previousHeight = await page.evaluate(scrollHeight);
-//   await page.evaluate(`window.scrollTo(0, ${scrollHeight})`);
-//   await page.waitForFunction(`${scrollHeight} > ${previousHeight}`, {
-//     timeout: 30000,
-//   });
-// };
-
-// const goToPostPageAndGetInfo = async (page) => {
-//   const result = await page.evaluate(() => {
-//     const $ = window.$;
-//     const targetPost = $($("._9AhH0:not(.done)")[0]);
-//     targetPost.addClass("done");
-//     return {
-//       date: $("._1o9PC.Nzb55").attr("datetime").substring(0, 10),
-//       title: "",
-//       user: $(".FPmhX.nJAzx").text(),
-//       content: $($(".gElp9")[0]).find("span").text(),
-//       click: $(".Nm9Fw").find("span").text(),
-//       link: targetPost.closest("a").attr("href"),
-//     };
-//   });
-//   return result;
-// };
-
-// const puppeteer = require('puppeteer');
-
-// async function scrapeInfiniteScrollItems(url) {
-//   const browser = await puppeteer.launch({
-//     args: [
-//       '--enable-features=NavigatorContentUtils',
-//       '--enable-features=NavigatorContentSettings',
-//       '--enable-features=Geolocation',
-//     ]
-//   });
-
-// const context = await browser.createIncognitoBrowserContext();
-// const page = await context.newPage();
-
-// // 위치 정보를 설정합니다.
-// await page.setGeolocation({ latitude: 37.123, longitude: -122.456 });
-
-// await page.goto(url);
-
-//   // 나머지 코드를 유지합니다.
-// }
-
-// // 크롤링할 페이지의 URL을 전달합니다.
-// scrapeInfiniteScrollItems('https://example.com');
